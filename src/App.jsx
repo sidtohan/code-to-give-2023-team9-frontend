02ecc856e39f1e2e994005c033b693c1ea3a2704
Siddhant Tohan <siddhant.tohan@gmail.com>
2023-06-04 23:49:36 +0530

    header for chat page done

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './css/App.css';
+import ChatHeader from './components/ChatHeader';
+
+function App() {
+  return (
+    <div>
+      <ChatHeader />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/Chat.jsx
@@ -0,0 +1,7 @@
+import ChatHeader from "../components/ChatHeader";
+
+export default function Chat(){
+    return <div className="chat">
+        <ChatHeader />
+    </div>
+}

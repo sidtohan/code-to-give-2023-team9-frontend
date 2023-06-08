e2995a7cae33d6d682efb396ba1b9e8acd5dcb31
Siddhant Tohan <siddhant.tohan@gmail.com>
2023-06-09 00:01:47 +0530

    Made ending page responsive and landing page also

--- src/pages/EndingPage.jsx
+++ src/pages/EndingPage.jsx
@@ -1,17 +1,24 @@
+import { motion } from "framer-motion";
 import Header from "../components/Header.jsx";
 import Image from "../components/Image.jsx";
 import Contact from "../components/Contact.jsx";
 import Fact from "../components/Fact.jsx";
 import "../css/ending_page.css";
 
-function EndingPage() {
+function EndingPage({ variants }) {
   return (
-    <div className="container-ending">
+    <motion.div
+      className="container-ending"
+      variants={variants}
+      initial="initial"
+      exit="exit"
+      animate="animate"
+    >
       <Header></Header>
       <Image></Image>
       <Contact></Contact>
       <Fact></Fact>
-    </div>
+    </motion.div>
   );
 }
 
